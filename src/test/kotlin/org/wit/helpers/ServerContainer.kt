fcd001e716f4fed74a6ec2f5dfd32649b3658060
Hanuma Satya Sunnam <hanuma.sathya@gmail.com>
2021-11-30 11:16:36 +0000

    unit testing...

--- src/test/kotlin/org/wit/helpers/ServerContainer.kt
+++ src/test/kotlin/org/wit/helpers/ServerContainer.kt
@@ -1,13 +1,2 @@
 package org.wit.helpers
 
-import org.wit.config.JavalinConfig
-
-object ServerContainer {
-
-    val instance by lazy {
-        startServerContainer()
-    }
-
-    private fun startServerContainer() = JavalinConfig().startJavalinService()
-
-}
