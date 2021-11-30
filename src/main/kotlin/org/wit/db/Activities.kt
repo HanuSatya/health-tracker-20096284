32c240767e1e586488e3e46c30314cd9b76afb35
Hanuma Satya Sunnam <hanuma.sathya@gmail.com>
2021-11-30 11:53:05 +0000

    unit testing....

--- src/main/kotlin/org/wit/db/Activities.kt
+++ src/main/kotlin/org/wit/db/Activities.kt
@@ -3,8 +3,7 @@ package org.wit.db
 import org.jetbrains.exposed.sql.ReferenceOption
 import org.jetbrains.exposed.sql.Table
 
-// SRP - Responsibility is to manage one activity.
-//       Database wise, this is the table object.
+
 
 object Activities : Table("activities") {
     val id = integer("id").autoIncrement().primaryKey()
